95e6ad4f67d85175bb77d0a60d4508caa91b1d0f
cheikh-kanteye <cheikhkanteye.contact@gmail.com>
2025-07-04 13:14:55 +0000

    add vercel config

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,21 @@
+import { VercelRequest, VercelResponse } from '@vercel/node';
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from '../src/app.module'; // adapte le chemin si besoin
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as express from 'express';
+import { createServer, Server } from 'http';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+let server: Server;
+
+async function bootstrap(): Promise<Server> {
+  const expressApp = express();
+  const app = await NestFactory.create(
+    AppModule,
+    new ExpressAdapter(expressApp),
+  );
+
   const config = new DocumentBuilder()
     .setTitle('SarayaTech Solution mailing API')
     .setDescription(
@@ -13,18 +24,31 @@ async function bootstrap() {
     .setVersion('1.0')
     .build();
 
-  try {
-    const document = SwaggerModule.createDocument(app, config);
-    SwaggerModule.setup('api', app, document, {
-      swaggerOptions: {
-        persistAuthorization: true,
-        docExpansion: 'none',
-      },
-    });
-  } catch (error) {
-    console.error('Swagger error:', error);
-  }
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document, {
+    swaggerOptions: {
+      persistAuthorization: true,
+      docExpansion: 'none',
+    },
+  });
+
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(process.env.PORT ?? 3000);
+  await app.init(); // pas app.listen() ici
+  return createServer(expressApp);
 }
-bootstrap();
+
+export default function handler(req: VercelRequest, res: VercelResponse) {
+  if (!server) {
+    bootstrap()
+      .then((createdServer) => {
+        server = createdServer;
+        server.emit('request', req, res);
+      })
+      .catch((err) => {
+        console.error('Error in handler:', err);
+        res.status(500).send('Internal Server Error');
+      });
+  } else {
+    server.emit('request', req, res);
+  }
+}
