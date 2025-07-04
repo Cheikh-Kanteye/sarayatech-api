2ecc8000428bac00c014010d09c91c248aa91197
cheikh-kanteye <cheikhkanteye.contact@gmail.com>
2025-07-04 14:36:59 +0000

    add vercel config

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,10 @@
-import { VercelRequest, VercelResponse } from '@vercel/node';
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from '../src/app.module'; // adapte le chemin si besoin
+import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
-import { ExpressAdapter } from '@nestjs/platform-express';
-import * as express from 'express';
-import { createServer, Server } from 'http';
 
-let server: Server;
-
-async function bootstrap(): Promise<Server> {
-  const expressApp = express();
-  const app = await NestFactory.create(
-    AppModule,
-    new ExpressAdapter(expressApp),
-  );
-
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, { cors: true });
   const config = new DocumentBuilder()
     .setTitle('SarayaTech Solution mailing API')
     .setDescription(
@@ -24,31 +13,18 @@ async function bootstrap(): Promise<Server> {
     .setVersion('1.0')
     .build();
 
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document, {
-    swaggerOptions: {
-      persistAuthorization: true,
-      docExpansion: 'none',
-    },
-  });
-
-  app.useGlobalPipes(new ValidationPipe());
-  await app.init(); // pas app.listen() ici
-  return createServer(expressApp);
-}
-
-export default function handler(req: VercelRequest, res: VercelResponse) {
-  if (!server) {
-    bootstrap()
-      .then((createdServer) => {
-        server = createdServer;
-        server.emit('request', req, res);
-      })
-      .catch((err) => {
-        console.error('Error in handler:', err);
-        res.status(500).send('Internal Server Error');
-      });
-  } else {
-    server.emit('request', req, res);
+  try {
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document, {
+      swaggerOptions: {
+        persistAuthorization: true,
+        docExpansion: 'none',
+      },
+    });
+  } catch (error) {
+    console.error('Swagger error:', error);
   }
+  app.useGlobalPipes(new ValidationPipe());
+  await app.listen(process.env.PORT ?? 3000);
 }
+bootstrap();
